cb074c7c9f1880fc2f4892810001d3150242aa6b
Mabsky <grimdango@gmail.com>
2025-02-18 19:31:21 +1100

    base game complete

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,20 +1,24 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public float Flapstrength; 
+    public float Flapstrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameObject.name = "Bird";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.linearVelocity = Vector2.up * Flapstrength;
             myRigidbody.rotation = 20;
@@ -22,7 +26,11 @@ public class BirdScript : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.Space) == true)
         {
             myRigidbody.rotation = 0;
-        }
-        
+        }       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # Unity 1st Project (Flappy Bird Clone)
 
+<img src="animate.gif" width = "500">
 
 ## Summary
-Based on Game Maker's Toolkit - The Unity Tutorial For Complete Beginners.
-https://www.youtube.com/watch?v=XtQMytORBmM&t=1044s
+Based on, and thanks to,</br> 
+**Game Maker's Toolkit - The Unity Tutorial For Complete Beginners.**</br>
+https://www.youtube.com/watch?v=XtQMytORBmM&t=1044s</br>
 
-With personal sprites/scripts/modifications. 
+With some personal sprites/scripts/modifications. 
 
 
 ## Log
 
+### 18/02/2025
+Game complete!</br>
+Possible future add-ons: </br>
+pause</br>
+sound</br>
+clouds/bg layer</br>
+wings</br>
+title screen</br>
+save high score</br>
+embed into webpage</br>
+
 ### 17/02/2025
-Done: sprites, basic pipe generation, bounce animation
+Done: sprites, basic pipe generation, bounce animation </br>
 Added minor bird rotation and collisions. 
 
 ### 17/02/2025
